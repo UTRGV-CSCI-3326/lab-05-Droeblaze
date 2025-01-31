d8af662ee952093648c6391a273f520f90ea26ef
Randy Cantu <89747309+Droeblaze@users.noreply.github.com>
2025-01-31 22:42:40 +0000

    Assignment 5

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,25 @@
+public class Input{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner (System.in);
 
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.print("What is your age? ");
+        int age = scanner.nextInt();
+
+        System.out.print("What is your weight (in pounds)? ");
+        double weight = scanner.nextDouble();
+
+        System.out.print("Are you a smoker (true or false)? ");
+        boolean isSmoker = scanner.nextBoolean();
+
+        System.out.println("Patient's name: " + name);
+        System.out.println("Patient's age: " + age);
+        System.out.println("Patient's weight: " + weight + " kg");
+        System.out.println("Patient is a smoker: " + isSmoker);
+
+        scanner.close();
+    }
+}
